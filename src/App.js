9d8ed71e33fb43c6ca1d8a00107a952b8ebce0d8
sujanshaikh369 <sush11112011@gmail.com>
2022-01-01 22:02:35 +0600

    Sujan Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Fixician</h1>
+      <h1>Fixician</h1>
+      <h2>MD. SUJAN SHAIKH</h2>
     </div>
   );
 }
