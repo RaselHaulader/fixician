c0fad384594fff503cd8f090380e737ee4ea3d70
RaselHaulader <hauladerrasel2@gmail.com>
2022-01-02 00:11:10 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Fixician</h1>
-     <h3>rasel</h3>
-     <h3>rasel haulader</h3>
+      <h1>Fixician</h1>
+      <h2>MD. SUJAN SHAIKH</h2>
+      <h2>Salman</h2>
+      <h3>rasel</h3>
     </div>
   );
 }
