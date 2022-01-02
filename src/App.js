f02569a8741bbd09607afbbc0ef47f66d2323392
RajuM1997 <mraju5797@gmail.com>
2022-01-02 23:14:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
+import AboutUs from "./pages/AboutUs/AboutUs";
 import Home from "./pages/Home/Home/Home";
-
-
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <AboutUs />
     </div>
   );
 }
