62f3d7ff45511ed09fff03a6bf6484a247aa15c0
RaselHaulader <hauladerrasel2@gmail.com>
2022-01-02 22:02:36 +0600

    adde

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Explore from '../../Explore/Explore';
+import Navbar from '../../Shared/Navbar/Navbar';
 import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
+            <Navbar>z</Navbar>
             <Services></Services>
             <Explore></Explore>
             <Experts></Experts>
