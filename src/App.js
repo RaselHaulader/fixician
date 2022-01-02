6a1b93d92c030b29dea14a7c0fd9dac1fc92b7b4
RaselHaulader <hauladerrasel2@gmail.com>
2022-01-02 15:51:55 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import Services from "./pages/Home/Services/Services";
+import Home from "./pages/Home/Home/Home";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Fixician</h1>
-      <h2>MD. SUJAN SHAIKH</h2>
-      <h2>Salman</h2>
-      <Services></Services>
+     <Home></Home>
     </div>
   );
 }
