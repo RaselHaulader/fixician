987dc99a85bf9f09ffe42167ad78b06d94bc7e82
RaselHaulader <hauladerrasel2@gmail.com>
2022-01-03 02:01:46 +0600

    added

--- src/pages/Home/Counter/Counter.js
+++ src/pages/Home/Counter/Counter.js
@@ -7,28 +7,28 @@ const Counter = () => {
     return (
         <div className='counter-container w-100 py-5'>
             <div className='counter-header text-center mb-5'>
-                <h1>We are done</h1>
+                <h1>Counter</h1>
             </div>
             <div className='counter-items-container p-3'>
                 <div className='text-center row'>
                     <CounterItem
-                        end={100}
-                        title={'Service completed'}
+                        end={94}
+                        title={'Completed Order'}
                         info={'Lorem ipsum dolor sit amet consectetur adipisicing .'}
                     />
                     <CounterItem
-                        end={100}
-                        title={'Service completed'}
+                        end={52}
+                        title={'Masters of Repair'}
                         info={'Lorem ipsum dolor sit amet consectetur adipisicing .'}
                     />
                     <CounterItem
-                        end={100}
-                        title={'Service completed'}
+                        end={25}
+                        title={'Our Partner'}
                         info={'Lorem ipsum dolor sit amet consectetur adipisicing .'}
                     />
                     <CounterItem
-                        end={100}
-                        title={'Service completed'}
+                        end={230}
+                        title={'Active Users'}
                         info={'Lorem ipsum dolor sit amet consectetur adipisicing .'}
                     />
 
