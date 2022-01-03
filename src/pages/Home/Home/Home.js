8fb2b48eabb8327fa233c09382c1771d682413f9
RaselHaulader <hauladerrasel2@gmail.com>
2022-01-03 14:11:24 +0600

    added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Explore from '../../Explore/Explore';
-import Login from '../../Login/Login/Login';
-import Register from '../../Login/Register/Register';
-import Navbar from '../../Shared/Navbar/Navbar';
-import Counter from '../Counter/Counter';
-import Experts from '../Experts/Experts';
-import Services from '../Services/Services';
-
-const Home = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Register></Register>
-            <Login></Login>
-            <Counter></Counter>
-            <Services></Services>
-            <Explore></Explore>
-        </div>
-    );
-=======
 import React from "react";
 import Explore from "../../Explore/Explore";
 import Navbar from "../../Shared/Navbar/Navbar";
@@ -38,7 +16,6 @@ const Home = () => {
       <Explore></Explore>
     </div>
   );
->>>>>>> 0cc63921fe644b10b03d3dbbcf1c57562ead1477
 };
 
 export default Home;
