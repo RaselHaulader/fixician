0cc63921fe644b10b03d3dbbcf1c57562ead1477
RajuM1997 <mraju5797@gmail.com>
2022-01-03 13:05:46 +0600

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AboutUs from "./pages/AboutUs/AboutUs";
 import Home from "./pages/Home/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Explore from "./pages/Explore/Explore";
-import AboutUs from "./pages/AboutUs/AboutUs";
 
 function App() {
   return (
